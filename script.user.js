e1b7380fac17d3aac3d54a4f81c6dd6d191e59b9
一发精选 67373.net <141643587+67373net@users.noreply.github.com>
2025-01-08 15:58:26 +0800

    Create script.user.js

---
+++ script.user.js
@@ -0,0 +1,23 @@
+// ==UserScript==
+// @name        所有脚本
+// @namespace   lx288
+// @description 所有脚本集中管理
+// @version     0.0.1
+// @author      lx288
+// @match       *
+// @grant       GM_registerMenuCommand
+// @grant       GM_setClipboard
+// @grant       GM_download
+// @grant       GM_xmlhttpRequest
+// @grant       GM_setValue
+// @grant       GM_getValue
+// @grant       unsafeWindow
+// @grant       GM_download
+// @run-at      document-end
+// @license     MIT
+// ==/UserScript==
+
+(function() {
+    var localVar = "I am local 2";
+    console.log(localVar); // 输出: I am local
+})();
