017fd23eb62dc959d07ec5449c6e331311d2a5c4
一发精选 67373.net <141643587+67373net@users.noreply.github.com>
2025-01-17 21:41:17 +0800

    add read anchor purple

--- script.user.js
+++ script.user.js
@@ -2,7 +2,7 @@
 // @name        所有脚本
 // @namespace   lx288
 // @description 所有脚本集中管理
-// @version     0.0.2
+// @version     0.0.3
 // @author      lx288
 // @match       *://*/*
 // @grant       GM_registerMenuCommand
@@ -13,12 +13,17 @@
 // @grant       GM_getValue
 // @grant       unsafeWindow
 // @grant       GM_download
+// @grant       GM_addStyle
 // @run-at      document-end
 // @license     MIT
 // ==/UserScript==
 // https://greasyfork.org/en/scripts/523180
 
 (function() {
-    var localVar = "I am local 4";
-    console.log(localVar); // 输出: I am local
+    'use strict'; console.log("version 1"); // 输出: I am local
+    GM_addStyle(`
+        a:visited {
+            color: purple !important;
+        }
+    `);
 })();
