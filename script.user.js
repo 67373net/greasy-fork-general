bc81d9b95d3c479b58ee956761fbe5407c44459b
一发精选 67373.net <141643587+67373net@users.noreply.github.com>
2025-01-19 14:37:05 +0800

    延迟执行

--- script.user.js
+++ script.user.js
@@ -2,7 +2,7 @@
 // @name        所有脚本
 // @namespace   lx288
 // @description 所有脚本集中管理
-// @version     0.0.6
+// @version     0.0.7
 // @author      lx288
 // @match       *://*/*
 // @grant       GM_registerMenuCommand
@@ -18,14 +18,10 @@
 // https://greasyfork.org/en/scripts/523180
 
 console.log("version 1");
+
 (function () {
     'use strict';
-
-
-    // 🟧 m-team
-    if(location.href.match(/m-team\./)) {
-        document.querySelector('a[href="/profile/detail/325208"] strong').innerText = "niming";
-    }
+    setTimeout(delayFunc, 5888);
 
     // 🟧 已读链接紫色
     function addStyle(styleStr) {
@@ -37,4 +33,12 @@ console.log("version 1");
           color: purple !important;
         }`)
 })();
+
+function delayFunc(){
+    // 🟧 m-team
+    if(location.href.match(/m-team\./)) {
+        document.querySelector('a[href="/profile/detail/325208"] strong').innerText = "niming";    
+    }
+}
+
 console.log("--- end ---");
