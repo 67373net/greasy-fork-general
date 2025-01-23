9f8cb4a71b093fa085291fe8b9f972e5426da484
一发精选 67373.net <141643587+67373net@users.noreply.github.com>
2025-01-23 21:12:31 +0800

    添加公众号功能

--- script.user.js
+++ script.user.js
@@ -17,28 +17,63 @@
 // ==/UserScript==
 // https://greasyfork.org/en/scripts/523180
 
-console.log("version 1");
+// 🟢 微信表情包批量下载
+urlTrigger(/sticker\.weixin\.qq\.com/, wxStickerDownload);
+// 🟢 公众号后台图片下载
+urlTrigger(/mp\.weixin\.qq\.com/, wxConsoleStickerDownload);
+// 🟢 m-team 馒头去掉名字
+urlTrigger(/[\/\.]m-team\./, mtDelName);
+// 🟩 已读链接紫色
+addStyle(`a:visited, a:visited * {
+  color: purple !important;
+}`);
 
-(function () {
-    'use strict';
-    setTimeout(delayFunc, 5888);
-
-    // 🟧 已读链接紫色
-    function addStyle(styleStr) {
-        let visitedStyle = document.createElement('style');
-        visitedStyle.textContent = styleStr
-        document.head.appendChild(visitedStyle);
-    }
-    addStyle(`a:visited, a:visited * {
-          color: purple !important;
-        }`)
-})();
-
-function delayFunc(){
-    // 🟧 m-team
-    if(location.href.match(/m-team\./)) {
-        document.querySelector('a[href="/profile/detail/325208"] strong').innerText = "niming";    
-    }
+// 🟠 url mather
+function urlTrigger(urlReg, funcName) {
+  if (location.href.match(urlReg)) funcName();
 }
 
-console.log("--- end ---");
+// 🟠 addstyle
+function addStyle(styleStr) {
+  let visitedStyle = document.createElement("style");
+  visitedStyle.textContent = styleStr;
+  document.head.appendChild(visitedStyle);
+}
+
+// 🟧 微信表情包批量下载
+function wxStickerDownload() {
+  GM_registerMenuCommand("下载表情包1", () => {
+    let nodeList = document.querySelectorAll(".stiker_content_ele");
+    let title = document.querySelector(".stiker_head_msg_title").innerText;
+    for (let i = 0; i < nodeList.length; i++) {
+      GM_download(
+        nodeList[i].src,
+        title + "_" + String(i + 101).substring(1, 3),
+      );
+    }
+  });
+}
+
+// 🟧 公众号后台图片下载
+function wxConsoleStickerDownload() {
+  GM_registerMenuCommand("下载表情包2", () => {
+    let nodeList = document.querySelectorAll(".you .bubble_cont img");
+    for (let i = 0; i < nodeList.length; i++) {
+      console.log(nodeList[i].src);
+      GM_download(
+        nodeList[i].src,
+        "公众号后台图片下载" + (Math.random() + 100 + i) + ".gif",
+      );
+    }
+  });
+}
+
+// 🟧 m-team 馒头去掉名字
+function mtDelName() {
+  setTimeout(() => {
+    document.querySelector('a[href="/profile/detail/325208"]').remove();
+    GM_registerMenuCommand("馒头主页", function () {
+      window.location.href = "/profile/detail/325208";
+    });
+  }, 5888);
+}
