268fcfb2b9df2d554d3aa1d305476077e6cdc889
一发精选 67373.net <141643587+67373net@users.noreply.github.com>
2025-01-30 20:50:58 +0800

    加入markdown下载代码

--- index.js
+++ index.js
@@ -10,10 +10,12 @@
 // @grant       GM_xmlhttpRequest
 // @grant       GM_setValue
 // @grant       GM_getValue
+// @grant       GM_addStyle
 // @grant       unsafeWindow
 // @grant       GM_download
 // @run-at      document-end
 // @license     MIT
-// @require     file:/Volumes/ssd0/proj/greasy-fork-general/local.js
+// @require     file:/Volumes/ssd0/proj/greasy-fork-general/lx288.js
+// @require     file:/Volumes/ssd0/proj/greasy-fork-general/Easy_Web_Page_to_Markdown.js
 // ==/UserScript==
 // https://greasyfork.org/en/scripts/523180
